075861880078a490e8f64883678d9808d51a4fa9
afre <a.frex_b@hotmail.com>
2017-03-05 22:09:37 -0500

    Parte1Components

--- frontEndEjercicio10/src/app/app.module.ts
+++ frontEndEjercicio10/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { AgarreComponent } from './agarre/agarre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    AgarreComponent
   ],
   imports: [
     BrowserModule,
